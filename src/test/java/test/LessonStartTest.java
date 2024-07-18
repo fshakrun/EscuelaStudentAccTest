3901b56fda1e786b7e6d1ce97c8df82f49926c84
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2024-07-18 16:45:23 +0300

    Update LessonStartTest.java

--- src/test/java/test/LessonStartTest.java
+++ src/test/java/test/LessonStartTest.java
@@ -30,7 +30,7 @@ public class LessonStartTest {
     @BeforeAll
     public static void setup() throws Exception {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--headless", "--window-size=1920,1080");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(120));
